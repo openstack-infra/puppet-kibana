4205b66396618f7cda75411c80d9b748d39ea926
K Jonathan Harker <k.jonathan.harker@hp.com>
2015-06-02 16:24:30 -0700

    Add kibana3 support
    Add support for the javascript version of kibana.
    
    By default, this is done by serving both the kibana and also an
    elasticsearch proxy from the same location. Allowed GETs and POSTs
    for read-only access to elasticsearch are passed to a proxy and all
    other requests are served from the kibana source directory.
    
    An optional prefix, such as 'elasticsearch/', can be specified in which
    case the reverse proxy to elasticsearch will be served from this
    sub-path.
    
    Change-Id: I13f9dff0bbd6498a36dc75b026c9042a9bb05e8f

---
+++ manifests/js.pp
@@ -0,0 +1,54 @@
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Class to install kibana frontend to logstash.
+#
+class kibana::js (
+  $vhost_template              = 'kibana/dual-elasticsearch.vhost.erb',
+  $vhost_aliases               = [],
+  $vhost_name                  = $::fqdn,
+  $vhost_proxy_timeout         = '120',
+  $vhost_proxy_connect_timeout = '15',
+  $elasticsearch_url           = 'http://localhost:9200',
+  $elasticsearch_prefix        = '/',  # Must contain trailing /
+  $git_revision                = 'v3.1.2',
+) {
+
+  $base_path = "/opt/kibana/${git_revision}"
+
+  vcsrepo { $base_path:
+    ensure   => latest,
+    provider => 'git',
+    source   => 'https://github.com/elasticsearch/kibana.git',
+    revision => $git_revision,
+    owner    => 'www-data',
+  }
+
+  file { "${base_path}/src/config.js":
+    ensure    => present,
+    content   => template('kibana/config.js.erb'),
+    owner     => 'www-data',
+    require   => Vcsrepo[$base_path],
+    subscribe => Vcsrepo[$base_path],
+  }
+
+  apache::vhost { 'kibana':
+    docroot       => "${base_path}/src",
+    vhost_name    => $vhost_name,
+    serveraliases => $vhost_aliases,
+    port          => 80,
+    template      => $vhost_template,
+  }
+
+}
